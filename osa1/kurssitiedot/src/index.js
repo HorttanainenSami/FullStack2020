9aa274a5c2df4d51ef5ab70971b0fc0c7a06f1f8
Sami Horttanainen <sami1horttanainen@gmail.com>
2020-07-09 10:09:17 +0300

    1.5 step5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,26 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+	  const course = {
+	    name: 'Half Stack application development',
+	    parts: [
+	      {
+		name: 'Fundamentals of React',
+		exercises: 10
+	      },
+	      {
+		name: 'Using props to pass data',
+		exercises: 7
+	      },
+	      {
+		name: 'State of a component',
+		exercises: 14
+	      }
+	    ]
+	  }
 	return (
 		<>
 			<Header course={course} /> 
-			<Content parts={parts}/> 
-			<Total parts={parts}/>
+			<Content course={course}/> 
+			<Total course={course}/>
 		</>
 	)
 }
@@ -29,22 +31,22 @@ const App = () => {
 const Header=(props)=>{
 	return(
 		<div>
-			<h1>{props.course}</h1>
+			<h1>{props.course.name}</h1>
 		</div>
 	)
 }
 const Content =(props)=>{
 	return (
 		<div>
-			<Part p={props.parts[0]} />
-			<Part p={props.parts[1]} />
-			<Part p={props.parts[2]} />
+			<Part p={props.course.parts[0]} />
+			<Part p={props.course.parts[1]} />
+			<Part p={props.course.parts[2]} />
 		</div>
 	)
 }
 const Total = (props)=>{
 	let total=0;
-	for(let part of props.parts){
+	for(let part of props.course.parts){
 		console.log(part.exercises);
 		total+=part.exercises;
 	}
