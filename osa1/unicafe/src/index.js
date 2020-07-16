941a7fe4404b1a52bea7632a24570fef01b35497
Sami Horttanainen <sami1horttanainen@gmail.com>
2020-07-16 12:45:40 +0300

    unicafe step3

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = ({good,neutral,bad})=>{
+	let all= good+neutral+bad;
+	let average = (bad*(-1))+good;
+return(
+	<div>
+		<h1>Statistics</h1>
+		<p> Good {good}</p>
+		<p> Neutral {neutral} </p>
+		<p> Bad {bad} </p>
+		<p> All {all} </p>
+		<p> Average {all===0?0:average/all} </p>
+		<p> Positive {all===0?0:(good/all)*100}% </p>
+	</div>
+)
+
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-const [all, setAll]=useState(0)
-const [average, setAverage]=useState(0)
 const increaseGood=()=>{
 	setGood(good+1)
-	setAll(all+1)
-	setAverage(average+1)
 }
 const increaseNeutral=()=>{
 	setNeutral(neutral+1)
-	setAll(all+1)
 }
 const increaseBad=()=>{
 	setBad(bad+1)
-	setAll(all+1)
-	setAverage(average-1)
 }
 
 
@@ -30,13 +39,7 @@ const increaseBad=()=>{
 	<button onClick={increaseGood}>good</button>
 	<button onClick={increaseNeutral}>neutral</button>
 	<button onClick={increaseBad}>bad</button>
-	<h1>Statistics</h1>
-	<p> Good {good}</p>
-	<p> Neutral {neutral} </p>
-	<p> Bad {bad} </p>
-	<p> All {all} </p>
-	<p> Average {all===0?0:average/all} </p>
-	<p> Positive {all===0?0:(good/all)*100}% </p>
+	<Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
