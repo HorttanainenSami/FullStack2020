7683ef10aff5f5b29b574b0c46ecf341fc4e60c0
Sami Horttanainen <sami1horttanainen@gmail.com>
2020-07-20 21:04:01 +0300

    kurssitiedot erillinen moduuli

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,50 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Course=({course})=>(
-	<div>
-		<Header course={course} />
-		<Content course={course} />
-		<Total course={course} />
-	</div>
-)
-
-
-const Courses=({courses})=>(
-	courses.map(course=> 
-		<Course course={course} key={course.id} />)
-)
-
-const Header=({course})=>(
-	<div>
-		<h2>{course.name}</h2>
-	</div>
-)
-
-const Content =({course})=>(
-	<div>
-		{course.parts.map(part=>
-			 <Part part={part} key={part.id} />)}
-	</div>
-)
-
-const Total = ({course})=>{
-	let total=course.parts.reduce(
-		(acc, curValue)=>
-			 acc+curValue.exercises
-		 ,0);
-
-	return(
-		<div> 
-			<p><b>Total of {total} exercises </b></p>
-		</div>
-	)
-}
-const Part =({part})=>(
-		<div>
-			<p>{part.name} {part.exercises}</p>
-		</div>
-)
+import Courses from './components/Course'
 
 const App = () => {
 const courses = [
