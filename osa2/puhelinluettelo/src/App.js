fbdc0c827baf52d3270609fa99fcf888f81e0099
Sami Horttanainen <sami1horttanainen@gmail.com>
2020-08-07 13:24:20 +0300

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import Person from './components/Person'
 import  PersonForm from './components/PersonForm'
 import Filter from './components/Filter.js'
+import axios from 'axios'
 const App = () => {
-	const [ persons, setPersons] = useState([
-	    { name: 'Arto Hellas',number:'040-1231244' }
-	  ]) 
-	const [ newName, setNewName ] = useState('')
+	const [persons, setPersons] = useState([]) 
+	const [newName, setNewName ] = useState('')
 	const [newNumb,setNewNumb]=useState('')
 	const [filter, setFilter]=useState('');
+
 	const handleChange=(event)=>setNewName(event.target.value);
 	const handleNumb=(event)=>setNewNumb(event.target.value);
-	
-	const handleFiltChange=(event)=>{
-		setFilter(event.target.value);
-		console.log(event.target.value)
-	}
+	const handleFiltChange=(event)=> setFilter(event.target.value); 
+
 	const addName=(event)=>{
 		event.preventDefault();
 		if(persons.filter(person=>person.name.toLowerCase()===newName.toLowerCase()).length===0){
-		const personObject={
-			name:newName,
-			number:newNumb
-		}
-		setPersons(persons.concat(personObject));
+			const personObject={
+				name:newName,
+				number:newNumb
+			}
+			setPersons(persons.concat(personObject));
 		}
 		else{
-		window.alert(`${newName} was already added to phonebook`);	
+			window.alert(`${newName} was already added to phonebook`);	
 		}
 	}
+
+	useEffect(() =>{
+	console.log('Effect')	
+
+		axios.get('http://localhost:3001/persons')
+		.then(response=>{
+			setPersons(response.data);
+		})
+	}, []);
   return (
     <div>
       <h2>Phonebook</h2>
