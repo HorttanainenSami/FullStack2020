d3fd5fe6da9fce0072fe702ba6d464c69d95466b
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-04-24 13:44:40 +0300

    blogilista step2

---
+++ osa4/App.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI 
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
