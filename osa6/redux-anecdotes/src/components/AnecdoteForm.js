ce2ee286360edd55f23074ba821af10695ed7f89
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-05-19 11:47:02 +0300

    anecdootit ja backend, step2

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { newNote } from '../reducers/anecdoteReducer'
+import { newAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { setNotification, resetNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
   
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    dispatch(newNote(content))
-    dispatch(setNotification(`Creting ${content} note was success` ))
+    const newObject = await anecdoteService.createNew(content)
+    dispatch(newAnecdote(newObject))
+    dispatch(setNotification(`Creting ${content} anecdote was success` ))
     setTimeout( () => {
       dispatch(resetNotification())
     }, 5000)
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,43 +1,31 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
 export const incrementVote = (id) => {
   return {
     type: 'INCREMENT_VOTE',
     data:{ id }
   }
 }
-export const newNote = (content) => {
+export const newAnecdote = (content) => {
   return {
-    type: 'NEW_NOTE',
-    data: { content }
+    type: 'NEW_ANECDOTE',
+    data:  content,
   }
 }
-const initialState = anecdotesAtStart.map(asObject)
+export const initializeAnecdotes = (content) => {
+  return {
+    type: 'INITIALIZE_ANECDOTES',
+    data:  content ,
+  }
+}
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
 
   switch (action.type) {
     case 'INCREMENT_VOTE':
-      return state.map(note => note.id === action.data.id ? { ...note, votes: note.votes+=1} : note)  
-    case 'NEW_NOTE':
-      const newNote = asObject(action.data.content)
-      return state.concat(newNote)
+      return state.map(anecdote => anecdote.id === action.data.id ? { ...anecdote, votes: anecdote.votes+=1} : anecdote)  
+    case 'NEW_ANECDOTE':
+      return state.concat(action.data)
+    case 'INITIALIZE_ANECDOTES':
+      return action.data
     default:
       return state
   }
