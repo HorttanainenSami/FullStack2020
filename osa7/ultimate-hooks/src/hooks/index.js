e696be2264442b13281aab3d8ba89ba057ec8c22
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-05-25 08:15:35 +0300

    ultimate hooks

---
+++ osa7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect( () => {
+    axios.get(baseUrl).then(response => setResources(response.data))
+  }, [])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
+  }
+
+  const service = {
+    create
+  }
+
+  return [
+    resources, service
+  ]
+}
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
