8fad1bf17abc98613f93e11e7e20a52ac2d88c49
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-06-22 09:42:43 +0300

    Genren kirjat

--- osa8/osa_b/library-frontend/src/components/Books.js
+++ osa8/osa_b/library-frontend/src/components/Books.js
@@ -1,9 +1,19 @@
 import { useLazyQuery } from '@apollo/client'
 import React, { useState, useEffect } from 'react'
 import { ALL_BOOKS } from '../queries'
+function reducer(accumalator, current) {
+  let add = []
+  for(const genre in current){
+    if (!accumalator.includes(current[genre].toLowerCase())) {
+      add.push(current[genre].toLowerCase()) 
+    }
+  }
+  return add.length === 0 ? accumalator: accumalator.concat(add)
+}
 const Books = (props) => {
   const [books, setBooks] = useState([])
   const [getBooks, result] = useLazyQuery(ALL_BOOKS)
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     getBooks()
@@ -17,11 +27,27 @@ const Books = (props) => {
   if (!props.show) {
     return null
   }
-
+  const booksToShow = () => {
+    if(books.length === 0 ){
+      return books
+    }
+   return (
+     filter === '' 
+     ? books 
+     : books.filter(book => book.genres.includes(filter))
+   )
+  }
   return (
     <div>
       <h2>books</h2>
 
+      <div>
+      Filter by genre 
+        <select value={filter} onChange={({ target }) => setFilter(target.value)}>
+          <option value=''>- </option>
+          { books.map(book => book.genres).reduce(reducer ,[] ).map(genre => <option value={genre} key={genre}> {genre} </option>)}
+        </select>
+      </div>
       <table>
         <tbody>
           <tr>
@@ -33,7 +59,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {booksToShow().map(a =>
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
