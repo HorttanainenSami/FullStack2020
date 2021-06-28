659d14fb5950f93e0149f84f882af91d8077d34e
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-06-28 14:51:47 +0300

    Express

---
+++ osa9/First_exercises/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+const app = express()
+
+app.get('/hello', (_req, res) => { 
+  res.send('Hello Full Stack!')
+})
+
+
+
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
+
