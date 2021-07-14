9fdceb19ddacc3d6b208ab88e7b51f81e5b8d608
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-07-14 18:48:48 +0300

    9.15

--- osa9/front-end/src/App.tsx
+++ osa9/front-end/src/App.tsx
@@ -1,11 +1,76 @@
 import React from 'react';
-type Part = {
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CourseDescriptionPart {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CourseDescriptionPart {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+interface CourseDescriptionPart extends CoursePartBase {
+  description: string;
+}
+interface CourseSpecialPart extends CourseDescriptionPart {
+  type: 'special';
+  requirements: string[];
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+const Part = (part : CoursePart) => {
+  switch(part.type) {
+    case('normal'):
+      return(
+        <>
+          <div><b>{part.name} {part.exerciseCount}</b></div> 
+          <i>{part.description}</i>
+        </>);
+    case('groupProject'):
+      return(
+        <>
+          <div><b>{part.name} {part.exerciseCount}</b></div> 
+          <div>project exercises {part.groupProjectCount}</div>
+        </>);
+    case('submission'):
+      return(
+        <>
+          <div><b>{part.name} {part.exerciseCount}</b></div> 
+          <i> {part.description} </i>
+          <div>submit to {part.exerciseSubmissionLink}</div>
+        </>);
+    case('special'):
+      return(
+        <>
+          <div><b>{part.name} {part.exerciseCount} </b></div>
+          <i>{part.description}</i>
+        <div>Requirements: {part.requirements.reduce<string | JSX.Element>((prev, cur) => prev === '' ? cur: <>{prev}, {cur}</>, '')}</div>
+        </>);
+    default:
+      return assertNever(part);
+  }
+}
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`)
+}
+// this is the new coursePart variable
+interface Part {
   name: string,
   exerciseCount: number
 }
 type exerciseCount = Omit<Part, 'name'>
 interface ContentProps {
-  courseParts: Part []
+  courseParts: CoursePart []
 }
 interface CourseParts {
   exerciseCounts: exerciseCount []
@@ -14,7 +79,7 @@ const Header = (props: {courseName: string}): JSX.Element => <h1> {props.courseN
 const Content = (props: ContentProps): JSX.Element => {
   return(
     <>
-      {props.courseParts.map(part => <p key={part.name}> {part.name} {part.exerciseCount} </p>) }
+      {props.courseParts.map(part => <Part key={part.name} {...part} />) }
     </>
   )
 }
@@ -28,20 +93,40 @@ const Total = (props: CourseParts): JSX.Element => {
 }
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
   const exerciseCount = courseParts.map((part) => ( {exerciseCount: part.exerciseCount} ))
   return (
     <div>
